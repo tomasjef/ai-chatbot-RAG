28c6815be20afda61c4fdca189040e4d28cb11c9
Tomas Jefanovas <tomas.jefanovas@gmail.com>
2026-06-30 14:16:41 +0100

    Configure Heroku database and OpenAI env

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,3 +1,6 @@
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.dig(:openai, :api_key)
+  config.access_token =
+    ENV["OPENAI_API_KEY"].presence ||
+    ENV["OPENAI_ACCESS_TOKEN"].presence ||
+    Rails.application.credentials.dig(:openai, :api_key)
 end
